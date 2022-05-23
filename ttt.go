21e6e4f9efb103abf65916224c28f1d88a802bff
Luka Krmpotic <luka.krmpotic@cosylab.com>
2022-05-23 12:48:53 +0200

    misc

--- ttt.go
+++ ttt.go
@@ -9,18 +9,12 @@ import (
 
 var nplayers int
 
-func init() {
-	rand.Seed(time.Now().Unix())
-	flag.IntVar(&nplayers, "n", 1, "number of players")
-}
-
+type player int
 type board [9]player
 type game struct {
-	board board
 	turn  player
+	board board
 }
-
-type player int
 
 const (
 	O    = player(-1)
@@ -28,76 +22,114 @@ const (
 	X    = player(1)
 )
 
+const (
+	Red    = "\033[1;31m"
+	Green  = "\033[1;32m"
+	ClrRst = "\033[0m"
+)
+
+func init() {
+	rand.Seed(time.Now().Unix())
+	flag.IntVar(&nplayers, "n", 1, "number of players")
+}
+
 func NewGame() game {
 	return game{turn: X}
 }
 
-func (p player) Other() player {
+func (p player) other() player {
 	if p == X {
 		return O
 	}
 	return X
 }
 
+func (p player) String() string {
+	switch p {
+	case X:
+		return Green + "X" + ClrRst
+	case O:
+		return Red + "O" + ClrRst
+	default:
+		return " "
+	}
+}
+
+func (b board) full() bool {
+	for _, p := range b {
+		if p == none {
+			return false
+		}
+	}
+	return true
+}
+
+func (b board) won() bool {
+	return false ||
+		// rows
+		b[0] != none && b[0] == b[1] && b[0] == b[2] ||
+		b[3] != none && b[3] == b[4] && b[3] == b[5] ||
+		b[6] != none && b[6] == b[7] && b[6] == b[8] ||
+		// columns
+		b[0] != none && b[0] == b[3] && b[0] == b[6] ||
+		b[1] != none && b[1] == b[4] && b[1] == b[7] ||
+		b[2] != none && b[2] == b[5] && b[2] == b[8] ||
+		// diagonals
+		b[0] != none && b[0] == b[4] && b[0] == b[8] ||
+		b[2] != none && b[2] == b[4] && b[2] == b[6]
+}
+
 func (g *game) Move(n int) (ok bool) {
-	if n < 0 || n > 8 ||
-		g.board[n] != 0 || g.Over() {
+	if n < 0 || n > 8 || g.board[n] != none || g.Over() {
 		return false
 	}
 
-	g.board[n] = g.turn
-	g.turn = g.turn.Other()
+	g.board[n], g.turn = g.turn, g.turn.other()
 	return true
 }
 
 func (g *game) unMove(n int) {
-	g.board[n] = none
-	g.turn = g.turn.Other()
+	g.board[n], g.turn = none, g.turn.other()
 }
 
-func (g *game) MoveAI() {
-	w, d, l := g.analyze()
-	var move int
+func (g *game) MoveAI() (ok bool) {
+	w, d, l := g.Analyze()
 	switch {
 	case len(w) > 0:
-		move = w[rand.Intn(len(w))]
+		g.Move(w[rand.Intn(len(w))])
 	case len(d) > 0:
-		move = d[rand.Intn(len(d))]
+		g.Move(d[rand.Intn(len(d))])
 	case len(l) > 0:
-		move = l[rand.Intn(len(l))]
+		g.Move(l[rand.Intn(len(l))])
+	default:
+		return false
 	}
-	g.Move(move)
+	return true
 }
 
-// analyze returns which moves for the player who's turn it is
-// win & draw & lose
-func (g *game) analyze() (wins []int, draws []int, losses []int) {
+func (g *game) Analyze() (wins []int, draws []int, losses []int) {
 	if g.Over() {
 		return nil, nil, nil
 	}
-
-	for m := 0; m < 9; m++ {
+	for m := range g.board {
 		if ok := g.Move(m); !ok {
 			continue
 		}
-		if g.Won() {
-			wins = append(wins, m)
-			g.unMove(m)
-			continue
-		}
-		if g.Over() {
-			draws = append(draws, m)
-			g.unMove(m)
-			continue
-		}
-		w, d, l := g.analyze() // enemy
 		switch {
-		case len(w) > 0:
-			losses = append(losses, m)
-		case len(d) > 0:
-			draws = append(draws, m)
-		case len(l) > 0:
+		case g.board.won():
 			wins = append(wins, m)
+		case g.board.full():
+			draws = append(draws, m)
+		default:
+			w, d, l := g.Analyze() // enemy
+			switch {
+			case len(w) > 0:
+				losses = append(losses, m)
+			case len(d) > 0:
+				draws = append(draws, m)
+			case len(l) > 0:
+				wins = append(wins, m)
+			}
 		}
 		g.unMove(m)
 	}
@@ -105,45 +137,20 @@ func (g *game) analyze() (wins []int, draws []int, losses []int) {
 }
 
 func (g *game) Over() (gameOver bool) {
-	return g.board.Full() || g.Won()
-}
-
-func (g *game) Won() bool {
-	b := g.board
-	return false ||
-		// rows
-		b[0] != 0 && b[0] == b[1] && b[0] == b[2] ||
-		b[3] != 0 && b[3] == b[4] && b[3] == b[5] ||
-		b[6] != 0 && b[6] == b[7] && b[6] == b[8] ||
-		// columns
-		b[0] != 0 && b[0] == b[3] && b[0] == b[6] ||
-		b[1] != 0 && b[1] == b[4] && b[1] == b[7] ||
-		b[2] != 0 && b[2] == b[5] && b[2] == b[8] ||
-		// diagonals
-		b[0] != 0 && b[0] == b[4] && b[0] == b[8] ||
-		b[2] != 0 && b[2] == b[4] && b[2] == b[6]
+	return g.board.full() || g.board.won()
 }
 
 func (g *game) Winner() player {
-	if !g.Won() {
+	if !g.board.won() {
 		return none
 	}
-	// rely on keeping track of turns
-	return g.turn.Other()
+	return g.turn.other()
 }
 
-func (b board) Full() bool {
-	for i := 0; i < 9; i++ {
-		if b[i] == 0 {
-			return false
-		}
-	}
-	return true
-}
-func (g game) String() (s string) {
+func (g *game) Board() (s string) {
 	// show field number if empty
 	f := func(i int) string {
-		if g.board[i] == 0 {
+		if g.board[i] == none {
 			return fmt.Sprintf("%d", i)
 		}
 		return g.board[i].String()
@@ -158,47 +165,29 @@ func (g game) String() (s string) {
 	return s
 }
 
-func (p player) String() string {
-	const (
-		Red   = "\033[1;31m"
-		Green = "\033[1;32m"
-		Reset = "\033[0m"
-	)
-
-	switch p {
-	case X:
-		return Green + "X" + Reset
-	case O:
-		return Red + "O" + Reset
-	default:
-		return " "
-	}
-
-}
-
 func main() {
 	flag.Parse()
 	game := NewGame()
-	fmt.Print(game)
+	fmt.Print(game.Board())
 	for !game.Over() {
-		fmt.Println(game.analyze())
+		fmt.Println(game.Analyze())
 		if nplayers >= 1 {
 			var move int
 			fmt.Printf("> ")
 			fmt.Scanf("%d", &move)
-			if move == 9 { // easter-egg
-				game.MoveAI()
-			} else if ok := game.Move(move); !ok {
+			if ok := game.Move(move); !ok {
 				continue
 			}
 		}
 		if nplayers <= 1 {
 			game.MoveAI()
 		}
-		fmt.Print(game)
+		fmt.Print(game.Board())
 	}
 
 	if w := game.Winner(); w != none {
 		fmt.Printf("\nPlayer %s won\n", w)
+	} else {
+		fmt.Printf("Draw\n")
 	}
 }
