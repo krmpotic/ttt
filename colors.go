db5c04e754f59ba926edf7107e0c0c146dcd3140
Luka Krmpotic <luka.krmpotic@gmail.com>
2023-08-30 22:31:03 +0200

    move colors to own file

---
+++ colors.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	red    = "\033[1;31m"
+	green  = "\033[1;32m"
+	clrRst = "\033[0m"
+	clrScr = "\033[H\033[2J"
+)
--- main.go
+++ main.go
@@ -7,13 +7,6 @@ import (
 	"time"
 )
 
-const (
-	red    = "\033[1;31m"
-	green  = "\033[1;32m"
-	clrRst = "\033[0m"
-	clrScr = "\033[H\033[2J"
-)
-
 var nplayers int
 var depthAI int
 var turnAI bool
